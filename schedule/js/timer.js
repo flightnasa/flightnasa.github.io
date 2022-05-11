8d18b3c82e8b9ee05109c3001ea5054b9720d54d
flightnasa <60407255+flightnasa@users.noreply.github.com>
2022-05-11 08:45:50 -0500

    Update timer.js

--- schedule/js/timer.js
+++ schedule/js/timer.js
@@ -20,7 +20,7 @@ function period(t) {
 
     $(".per").html(period(secs)[1]);
     $(".bg").css("width", 100 - ((Math.floor(diff % 60) / .6) + ((diff % 1) / .6)) + "%");
-    document.title = `${hh}:${mm}:${ss} left...`
+    document.title = `🔔 ${hh}:${mm}:${ss} 🔔`
 
     setTimeout(tick, 25)
 }();
