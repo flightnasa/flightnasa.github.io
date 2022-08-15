1269696fb59fa6e994a21b9851f81999f71ed1a5
flightnasa <60407255+flightnasa@users.noreply.github.com>
2022-08-14 21:41:43 -0500

    Delete index directory

--- index/js/intro.js
+++
@@ -1,6 +0,0 @@
-var i = 0,
-arr = ["J","Ju","Jus","Jusr","Jus","Just","Justu","Just","Justi","Justin","Justin","Justin","Justin","Justin","Justin","Justin","Justin<a>.</a>"];
-setTimeout((function() {
-    var t = setInterval((function() {
-        i > arr.length && clearInterval(t), 
-        $("p").html(arr[i]),i++}), 150)}), 1e3);
