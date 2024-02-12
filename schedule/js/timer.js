0efee9caf8b48b756a37e6c387bae13aec491e36
flightnasa <60407255+flightnasa@users.noreply.github.com>
2024-02-11 21:08:06 -0600

    Add files via upload

--- schedule/js/timer.js
+++ schedule/js/timer.js
@@ -1,26 +1,20 @@
 function period(t) {
     for (i = 0; i < data.length; i++)
-        if (data[i][0] > t) return data[i]
+        if (data[i][0] * 3600 + data[i][1] * 60 + 5 > t) return data[i]
 }
 
 ! function tick() {
     start = new Date;
     secs = start.getMilliseconds() / 1e3 + start.getSeconds() + 60 * start.getMinutes() + 3600 * start.getHours(); 
 
-    diff = period(secs)[0] - secs;
-    hh = ("0" + Math.floor(diff / 3600)).slice(-2);
-    mm = ("0" + Math.floor(diff / 60 % 60)).slice(-2);
-    ss = ("0" + Math.floor(diff % 60)).slice(-2);
-    ms = ("000" + (diff % 1)).slice(-3);
+    diff = period(secs)[0] * 3600 + period(secs)[1] * 60 + 5 - secs;
+    
+    $("#time_left").html(new Date(diff * 1000).toISOString().slice(11, 19));
+    document.title = new Date(diff * 1000).toISOString().slice(11, 19);
 
-    $(".hh").html(`${hh}`);
-    $(".mm").html(`${mm}`);
-    $(".ss").html(`${ss}`);
-    $(".ms").html(`${ms}`);
+    $("#time_left--period").html("CURRENT PERIOD: " + period(secs)[2]);
 
-    $(".per").html(period(secs)[1]);
-    $(".bg").css("width", 100 - ((Math.floor(diff % 60) / .6) + ((diff % 1) / .6)) + "%");
-    document.title = `${hh}:${mm}:${ss}`
+    $("#progress_bar").css("width", 100 - ((Math.floor(diff % 60) / .6) + ((diff % 1) / .6)) + "%");
 
-    setTimeout(tick, 25)
+    setTimeout(tick, 25);
 }();
