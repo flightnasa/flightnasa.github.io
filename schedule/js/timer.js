7b15f0d0ca7d761348fd0f6e72f63a7f4c16dc1c
Justin <60407255+flightnasa@users.noreply.github.com>
2024-06-06 04:03:53 -0500

    Add files via upload

--- schedule/js/timer.js
+++ schedule/js/timer.js
@@ -1,52 +1,26 @@
-// determine what period it is
 function period(t) {
-     // sorcery
-     for (i = 0; i < periods.length; i++)
-          if (periods[i][0] * 3600 + periods[i][1] * 60 + 5 > t)
-               return periods[i];
+    for (i = 0; i < data.length; i++)
+        if (data[i][0] > t) return data[i]
 }
 
-!(function tick() {
-     // get seconds since start of day
-     start = new Date();
-     seconds =
-          start.getMilliseconds() / 1e3 +
-          start.getSeconds() +
-          60 * start.getMinutes() +
-          3600 * start.getHours();
+! function tick() {
+    start = new Date;
+    secs = start.getMilliseconds() / 1e3 + start.getSeconds() + 60 * start.getMinutes() + 3600 * start.getHours(); 
 
-     // calculate & format time left
-     timeLeft =
-          period(seconds)[0] * 3600 + period(seconds)[1] * 60 + 5 - seconds;
-     timeLeft >= 43200 ? (x = 11) : (x = 12);
-     timeLeftFormatted = new Date(timeLeft * 1000).toISOString().slice(x, 19);
+    diff = period(secs)[0] - secs;
+    hh = ("0" + Math.floor(diff / 3600)).slice(-2);
+    mm = ("0" + Math.floor(diff / 60 % 60)).slice(-2);
+    ss = ("0" + Math.floor(diff % 60)).slice(-2);
+    ms = ("000" + (diff % 1)).slice(-3);
 
-     // display time left
-     $("#time__left").html(timeLeftFormatted.replace(/:/g, "·"));
-     document.title = timeLeftFormatted;
+    $(".hh").html(`${hh}`);
+    $(".mm").html(`${mm}`);
+    $(".ss").html(`${ss}`);
+    $(".ms").html(`${ms}`);
 
-     // display current period
-     $("#time__period").html(period(seconds)[2]);
+    $(".per").html(period(secs)[1]);
+    $(".bg").css("width", 100 - ((Math.floor(diff % 60) / .6) + ((diff % 1) / .6)) + "%");
+    document.title = `${hh}:${mm}:${ss}`
 
-     // display greeting
-     greeting =
-          seconds <= 43200
-               ? "Good morning"
-               : seconds <= 61200
-               ? "Good afternoon"
-               : seconds <= 72000
-               ? "Good evening"
-               : "Good night";
-     $("#time__greeting").html(greeting);
-
-     // display current date
-     $("#time__date").html(
-          (start.getMonth() + 1).toString() +
-               "-" +
-               ("0" + start.getDate().toString()).substr(-2) +
-               "-" +
-               start.getFullYear().toString().substr(2)
-     );
-
-     setTimeout(tick, 500);
-})();
+    setTimeout(tick, 25)
+}();
